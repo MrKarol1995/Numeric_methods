e1e9f26e56e27439190d4e11edffacb0f1b4d460
Karol Cieślik <karolcieslik121@wp.pl>
2024-11-27 00:03:01 +0100

    Update Lista5.py

--- Lista5.py
+++ Lista5.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline, interp1d
 
-
 def quadratic_interpolation(h_values, rho_values): ##############
     """
     Aproksymacja kwadratowa dla funkcji rho(h) na podstawie podanych danych.
